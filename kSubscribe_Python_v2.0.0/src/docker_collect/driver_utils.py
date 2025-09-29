cc69291a3819d70361c98b2ba2d97fcf755573fe
themiraclesoft <themiraclesoft@themiraclesoft.com>
2025-09-29 16:06:40 +0900

    fix: comment out driver

--- kSubscribe_Python_v2.0.0/src/docker_collect/driver_utils.py
+++ kSubscribe_Python_v2.0.0/src/docker_collect/driver_utils.py
@@ -11,48 +11,45 @@ import os
 import subprocess
  
 
-
-
-
-
-def get_driver2(): #git에서 다운받은 SELENIUM_LAYER.zip을 사용했다면, 아래 설정 중 아무것도 건드리지 않는다. 
-    # chrome_options = Options()
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--window-size=1280x1696')
-    # chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-    # chrome_options.add_argument('--hide-scrollbars')
-    # chrome_options.add_argument('--enable-logging')
-    # chrome_options.add_argument('--log-level=0')
-    # chrome_options.add_argument('--v=99')
-    # chrome_options.add_argument('--single-process')
-    # chrome_options.add_argument('--data-path=/tmp/data-path')
-    # chrome_options.add_argument('--ignore-certificate-errors')
-    # chrome_options.add_argument('--homedir=/tmp')
-    # chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-    # chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    # # chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
-    # # 이미지 로딩 X
-    # chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-    # chrome_options.binary_location = "/opt/python/bin/headless-chromium" 
+# 14~51
+# def get_driver2(): #git에서 다운받은 SELENIUM_LAYER.zip을 사용했다면, 아래 설정 중 아무것도 건드리지 않는다. 
+#     # chrome_options = Options()
+#     # chrome_options.add_argument('--headless')
+#     # chrome_options.add_argument('--no-sandbox')
+#     # chrome_options.add_argument('--disable-gpu')
+#     # chrome_options.add_argument('--window-size=1280x1696')
+#     # chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+#     # chrome_options.add_argument('--hide-scrollbars')
+#     # chrome_options.add_argument('--enable-logging')
+#     # chrome_options.add_argument('--log-level=0')
+#     # chrome_options.add_argument('--v=99')
+#     # chrome_options.add_argument('--single-process')
+#     # chrome_options.add_argument('--data-path=/tmp/data-path')
+#     # chrome_options.add_argument('--ignore-certificate-errors')
+#     # chrome_options.add_argument('--homedir=/tmp')
+#     # chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+#     # chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+#     # # chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
+#     # # 이미지 로딩 X
+#     # chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+#     # chrome_options.binary_location = "/opt/python/bin/headless-chromium" 
     
-    # driver = webdriver.Chrome('/opt/python/bin/chromedriver', chrome_options=chrome_options)
-    # # driver.set_page_load_timeout(30)
+#     # driver = webdriver.Chrome('/opt/python/bin/chromedriver', chrome_options=chrome_options)
+#     # # driver.set_page_load_timeout(30)
 
 
-    # window 버전
-    # 옵션 설정
-    options = Options()
-    # user-agent 설정
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-    options.add_argument('user-agent=' + user_agent)
-    options.add_argument('Content-Type=application/json; charset=utf-8')
-    options.add_argument('--blink-settings=imagesEnabled=false')
-    # options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다
+#     # window 버전
+#     # 옵션 설정
+#     options = Options()
+#     # user-agent 설정
+#     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+#     options.add_argument('user-agent=' + user_agent)
+#     options.add_argument('Content-Type=application/json; charset=utf-8')
+#     options.add_argument('--blink-settings=imagesEnabled=false')
+#     # options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다
 
-    driver = webdriver.Chrome(executable_path='C:\chromedriver_win32\chromedriver.exe', chrome_options=options)
-    return driver
+#     driver = webdriver.Chrome(executable_path='C:\chromedriver_win32\chromedriver.exe', chrome_options=options)
+#     return driver
 
 # NEW
 # def get_driver():
